58e3692dc13324a2048343ad93554b15fe551abb
andres <gokutanve@gmail.com>
2024-04-16 13:36:55 -0400

    first version of linkedlists

---
+++ script.rb
@@ -0,0 +1,103 @@
+require 'pry-byebug'
+
+class Node
+  attr_accessor :next
+  attr_reader :value
+
+  def initialize(value=nil)
+    @value = value
+    @next = nil
+  end
+
+  def next_node(node)
+    @next = node
+  end
+end
+
+class LinkedList
+  attr_reader :head, :tail
+  def initialize
+    @head = nil
+    @tail = nil
+  end
+
+  def append(node)
+    if @head.nil?
+      @head = node
+    elsif @tail.nil?
+      @head.next = node
+      @tail = node
+    else
+      @tail.next = node
+      @tail = node
+    end
+  end
+
+  def prepend(node)
+    if @head.nil?
+      @head = node
+    elsif @tail.nil?
+      @tail = @head
+      @head = node
+      @head.next = @tail
+    else
+      node.next = @head
+      @head = node
+    end
+  end
+
+  def size
+    i = 0
+    current_node = @head
+    until current_node.nil?
+      i += 1
+      current_node = current_node.next
+    end
+    i
+  end
+
+  def at(index)
+    return nil if index > size
+    current_node = @head
+    index.times do
+      current_node = current_node.next
+    end
+    current_node
+  end
+
+  def pop
+    n = self.size
+    @tail = nil
+    self.at(n-2).next = nil
+    @tail = self.at(n-2)
+  end
+
+  def contains?(value)
+    0.upto(self.size) do |i|
+      return false if self.at(i).nil?
+      return true if value == self.at(i).value
+    end
+  end
+
+  def find(value)
+    0.upto(self.size) do |i|
+      return nil if self.at(i).nil?
+      return i if value == self.at(i).value
+    end
+  end
+
+  def to_s
+    string = "#{@head.value}"
+    1.upto((self.size) - 1) do |i|
+      string << " -> #{self.at(i).value}"
+    end
+    string
+  end
+end
+a = Node.new("A")
+b = Node.new("B")
+c = Node.new("C")
+d = Node.new('D')
+e = Node.new('E')
+binding.pry
+list = LinkedList.new
